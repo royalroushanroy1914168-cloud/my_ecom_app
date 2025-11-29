7ce5a94e79aa3bb73a4c8e6cc976ebf47e5ce78c
royalroushanroy1914168-cloud <royalroushanroy1914168@gmail.com>
2025-11-29 23:45:41 +0530

    Initialize SQLite database connection in db.js

--- db.js
+++ db.js
@@ -1,1 +1,9 @@
+// db.js
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+const dbFile = path.join(__dirname, 'database.sqlite');
+const db = new sqlite3.Database(dbFile);
+
+module.exports = db;
 
